1e448c61811e0203987d805b6e1506c5e3283288
benjaerospace <dev.benjaminpaumard@gmail.com>
2022-03-12 11:56:25 +0100

    22.03.12 - adding home page
    The page has not been html, css nor xhtml checked

---
+++ src/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="author" content="Lucas L, Benjamin P" />
+    <meta name="description" content="La musique, sans l'écoute." />
+    <link rel="stylesheet" href="./css/style.css" />
+    <link rel="icon" href="./images/favicon.ico" />
+    <title>Spotifail</title>
+</head>
+<body>
+    <main>
+        <h1><img src="./images/spotifail.png" alt="spotifail logo" height="55"> Spotifail</h1>
+        <h2>La musique, sans l'écoute.</h2>
+    </main>
+</body>
+</html>
